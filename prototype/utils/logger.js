be678117bf5afd1313a6e6d52993cd7956c737de
Akash Sharma <officialmailakashsharma@gmail.com>
2025-09-11 17:15:04 +0000

    Add simple file logger

---
+++ prototype/utils/logger.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+
+function log(message) {
+    const timestamp = new Date().toISOString();
+    fs.appendFileSync('app.log', `[${timestamp}] ${message}\n`);
+    console.log(`[${timestamp}] ${message}`);
+}
+
+module.exports = { log };
